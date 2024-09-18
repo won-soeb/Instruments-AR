2cf50bf371b8e59404fddc67a79e292daf844eb2
SWS\wsinp <wsinp431@naver.com>
2024-09-19 04:10:43 +0900

    악기 배치 - 테스트

--- Assets/Scripts/NoteManager.cs
+++ Assets/Scripts/NoteManager.cs
@@ -2,10 +2,16 @@ using UnityEngine;
 
 public class NoteManager : MonoBehaviour
 {
+    public enum InstrumentType { Piano, Drum, Bell }//악기 종류
+    public InstrumentType type;
     private AudioSource audio;//연주 사운드
     private ParticleSystem particle;//이펙트
     private MeshRenderer renderers;
-    public Material[] materials;//인식 표시
+    //인식 표시
+    [Header("피아노, 드럼")]
+    public Material[] materials;
+    [Header("벨")]
+    public Animator anim;
 
     private void Awake()
     {
@@ -15,13 +21,21 @@ public class NoteManager : MonoBehaviour
     }
     public void CheckNote(bool check)
     {
-        if (check)
+        //악기 종류에 따라 다른 로직
+        if (type == InstrumentType.Bell)
         {
-            renderers.material = materials[1];
+            anim.SetBool("IsFocusing", check);
         }
         else
         {
-            renderers.material = materials[0];
+            if (check)
+            {
+                renderers.material = materials[1];
+            }
+            else
+            {
+                renderers.material = materials[0];
+            }
         }
     }
     public void PlayeNote(bool note)
--- Assets/Scripts/PlaneManager.cs
+++ Assets/Scripts/PlaneManager.cs
@@ -1,50 +1,94 @@
-using System;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 using UnityEngine.XR.ARFoundation;
 using UnityEngine.XR.ARSubsystems;
 
 public class PlaneManager : MonoBehaviour
 {
-    public GameObject testPlane;
-    public GameObject realPlane;
-    private GameObject testPlaneGO;
-    private GameObject realPlaneGO;
+    public GameObject indicator;
+    public GameObject instrument;
+    private GameObject instrumentPos;
+    ARRaycastManager raycastManager;
 
-    private ARRaycastManager arRayManager;
-    private ARPlaneManager arManager;
 
     private void Awake()
     {
-        arRayManager = GetComponent<ARRaycastManager>();
+        indicator.SetActive(false);
+        raycastManager = GetComponent<ARRaycastManager>();
     }
     private void Start()
     {
-        testPlaneGO = Instantiate(testPlane);
-        realPlaneGO = Instantiate(realPlane);
     }
     private void Update()
     {
-        Tracking();
+        DetectGround();
+
+        if (indicator.activeInHierarchy && Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            Rotate(touch);
+            // 터치된 위치의 UI 요소를 검색
+            PointerEventData pointerEventData = new PointerEventData(EventSystem.current);
+            pointerEventData.position = touch.position;
+            var results = new List<RaycastResult>();
+            EventSystem.current.RaycastAll(pointerEventData, results);
+
+            if (results.Count > 0)
+            {
+                // UI 요소에 터치됨
+                Debug.Log("Touched UI element.");
+            }
+            else
+            {
+                // UI 요소에 터치되지 않음
+                if (instrumentPos == null)
+                {
+                    instrumentPos = Instantiate(instrument, indicator.transform.position, indicator.transform.rotation);
+                }
+                else
+                {
+                    if (Vector3.Distance(instrumentPos.transform.position, indicator.transform.position) > 1)
+                    {
+                        instrumentPos.transform.SetPositionAndRotation(indicator.transform.position, indicator.transform.rotation);
+                    }
+                }
+            }
+        }
     }
-    private void Tracking()
+    Vector2 screenSize;
+    private void DetectGround()
     {
-        Vector2 screenSize = new Vector2(Screen.width * 0.5f, Screen.height * 0.5f);
+        screenSize = new Vector2(Screen.width * 0.5f, Screen.height * 0.5f);
         List<ARRaycastHit> hitInfos = new List<ARRaycastHit>();
 
-        if (arRayManager.Raycast(screenSize, hitInfos, TrackableType.Planes))
+        if (raycastManager.Raycast(screenSize, hitInfos, TrackableType.Planes))
         {
-            testPlaneGO.transform.position = hitInfos[0].pose.position;
-            testPlaneGO.transform.rotation = hitInfos[0].pose.rotation;
-            
-            realPlaneGO.transform.position = hitInfos[0].pose.position;
-            realPlaneGO.transform.rotation = hitInfos[0].pose.rotation;
-            realPlaneGO.SetActive(true);
-            Debug.Log(hitInfos[0].pose.position);
+            indicator.SetActive(true);
+            indicator.transform.position = hitInfos[0].pose.position;
+            indicator.transform.rotation = hitInfos[0].pose.rotation;
         }
         else
         {
-            realPlaneGO.SetActive(false);
+            indicator.SetActive(false);
+        }
+    }
+    private void Rotate(Touch touch)
+    {
+        if (touch.phase == TouchPhase.Moved)
+        {
+            // 현재 터치 위치와 이전 터치 위치 사이의 벡터를 구합니다.
+            Vector2 deltaPos = touch.deltaPosition;
+
+            // 회전 각도를 벡터의 x 방향으로 지정합니다.
+            float rotationAmount = deltaPos.x * -0.1f;
+
+            // 회전 축을 설정하여 car를 회전합니다.
+            if (instrumentPos != null)
+            {
+                instrumentPos.transform.Rotate(Vector3.up, rotationAmount);
+            }
         }
     }
 }
